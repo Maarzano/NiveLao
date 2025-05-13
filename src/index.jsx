7a7e0d23fb3727bc3d0ef92e0ed6aa973a225a23
Marzano <arthuramarzano10@gmail.com>
2025-05-13 16:31:09 -0300

    arrumando, adicionando componentes, retirando coisas inúteis etc.. limpando projeto

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import App from './Pages/Home/App.jsx'
+import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
